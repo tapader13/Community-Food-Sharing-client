cf93a41bb8b77194b91caaa8bd1bced058d7c2ce
tapader13 <minhajtapader0@gmail.com>
2024-12-21 23:45:35 -0500

    banner part design complete

---
+++ src/components/home/Banner.jsx
@@ -0,0 +1,75 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+const Banner = () => {
+  const slides = [
+    {
+      image: '/Variety-fruits-vegetables.webp',
+      title: 'Natural',
+      subtitle: 'Supplied by nature, grown by nature lovers.',
+      discount: 'Only Today 30% Discount!',
+      cta: 'Subscribe Today',
+    },
+    {
+      image: '/healthy-eating-ingredients-1296x728-header.jpg',
+      title: 'Healthy',
+      subtitle: 'Fresh from the farm to your table.',
+      discount: 'Only Today 30% Discount!',
+      cta: 'Montly Subscribe',
+    },
+    {
+      image: '/premium_photo-1670601440146-3b33dfcd7e17.avif',
+      title: 'Smarter',
+      subtitle: 'Eco-friendly choices for a better tomorrow.',
+      discount: 'Only Today 30% Discount!',
+      cta: 'Montly Subscribe',
+    },
+  ];
+
+  return (
+    <Swiper
+      spaceBetween={30}
+      centeredSlides={true}
+      autoplay={{
+        delay: 5000,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      modules={[Autoplay, Pagination, Navigation]}
+      className='mySwiper'
+    >
+      {slides.map((slide, index) => (
+        <SwiperSlide key={index}>
+          <div className='relative h-[600px] sm:h-[90vh] w-full'>
+            <img
+              src={slide.image}
+              alt={slide.title}
+              className='absolute inset-0 w-full h-full object-cover'
+            />
+            <div className='absolute inset-0 bg-black bg-opacity-40'></div>
+            <div className='absolute inset-0 flex flex-col p-10 text-white text-center'>
+              <div className='w-full flex justify-between items-start px-10'>
+                <p className='text-2xl abril font-semibold'>{slide.discount}</p>
+                <p className='text-2xl abril font-semibold'>{slide.cta}</p>
+              </div>
+              <div className='transform relative top-1/2 -translate-y-1/2'>
+                <h2 className='text-[80px] abril font-bold mb-1'>
+                  {slide.title}
+                </h2>
+                <p className='text-xl mb-6'>{slide.subtitle}</p>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default Banner;
