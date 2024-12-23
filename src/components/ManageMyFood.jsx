26dec36f0449e6b15217be5b846c17e092e37374
tapader13 <minhajtapader0@gmail.com>
2024-12-22 21:10:53 -0500

    my food page update food info using modal

--- src/components/ManageMyFood.jsx
+++ src/components/ManageMyFood.jsx
@@ -3,12 +3,21 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 import useAuth from '../hooks/useAuth';
 import Spinner from './Spinner';
+import DatePicker from 'react-datepicker';
+import { useForm } from 'react-hook-form';
 
 const ManageMyFood = () => {
   const { user } = useAuth();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const [foods, setFoods] = useState([]);
   const [loading, setLoading] = useState(false);
-
+  const [modalOpen, setModalOpen] = useState(false);
+  const [expiryDate, setExpiryDate] = useState(new Date());
+  const [food, setFood] = useState({});
   const fetchFoods = async () => {
     try {
       setLoading(true);
@@ -17,6 +26,7 @@ const ManageMyFood = () => {
       });
       if (res.data.success) {
         setFoods(res.data.data);
+        setExpiryDate(new Date(res.data?.data[0]?.expiryDate));
       }
     } catch (error) {
       toast.error(error.response.data.message);
@@ -36,11 +46,49 @@ const ManageMyFood = () => {
       }
     }
   };
-
+  const onSubmit = async (data) => {
+    try {
+      setLoading(true);
+      const res = await axios.patch(`http://localhost:5001/foods/${food._id}`, {
+        foodName: data.foodName,
+        foodImage: data.foodImage,
+        pickupLocation: data.pickupLocation,
+        additionalNotes: data.additionalNotes,
+        foodStatus: food.foodStatus,
+        foodQuantity: parseInt(data.foodQuantity),
+        donator: {
+          name: food.donator.name,
+          email: food.donator.email,
+          image: food.donator.image,
+        },
+        expiryDate: expiryDate.toISOString(),
+      });
+      if (res?.data?.success) {
+        const updateUi = foods.find((f) => f._id === food._id);
+        if (updateUi) {
+          updateUi.foodName = data.foodName;
+          updateUi.foodQuantity = parseInt(data.foodQuantity);
+          updateUi.expiryDate = new Date(expiryDate).toLocaleDateString();
+        }
+        toast.success(res.data.message);
+        setModalOpen(false);
+      }
+      //   navigate('/available-foods');
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      setLoading(false);
+    }
+  };
   useEffect(() => {
     fetchFoods();
   }, []);
   console.log(foods);
+  useEffect(() => {
+    if (food?.expiryDate) {
+      setExpiryDate(new Date(food.expiryDate));
+    }
+  }, [food]);
   return (
     <div className='w-11/12 mx-auto p-4'>
       <h2 className='text-2xl font-bold text-center mb-6'>
@@ -72,7 +120,10 @@ const ManageMyFood = () => {
                 </td>
                 <td className='border border-gray-300 px-4 py-2'>
                   <button
-                    onClick={() => console.log('Update', food)}
+                    onClick={() => {
+                      setModalOpen(true);
+                      setFood(food);
+                    }}
                     className='bg-green-600 text-white px-4 py-2 rounded mr-2'
                   >
                     Update
@@ -88,6 +139,187 @@ const ManageMyFood = () => {
             ))}
           </tbody>
         </table>
+      )}
+      {/* modal */}
+      {modalOpen && (
+        <div className='fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center'>
+          <div className='bg-white p-6 rounded shadow-lg max-w-md w-full'>
+            <h3 className='text-xl font-bold mb-4'>Update Food Info</h3>
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className='max-w-lg mx-auto space-y-4'
+            >
+              {/* Food Name */}
+              <div>
+                <label
+                  htmlFor='foodName'
+                  className='block text-sm font-medium text-gray-700'
+                >
+                  Food Name
+                </label>
+                <input
+                  type='text'
+                  id='foodName'
+                  defaultValue={food?.foodName}
+                  {...register('foodName', {
+                    required: 'Food name is required',
+                  })}
+                  className='w-full px-4 py-2 border border-gray-300 rounded'
+                />
+                {errors.foodName && (
+                  <p className='text-red-500 text-xs mt-1'>
+                    {errors.foodName.message}
+                  </p>
+                )}
+              </div>
+
+              {/* Food Image URL */}
+              <div>
+                <label
+                  htmlFor='foodImage'
+                  className='block text-sm font-medium text-gray-700'
+                >
+                  Food Image URL
+                </label>
+                <input
+                  type='text'
+                  id='foodImage'
+                  defaultValue={food?.foodImage}
+                  {...register('foodImage', {
+                    required: 'Food image URL is required',
+                    pattern: {
+                      value:
+                        /^(https?:\/\/)?(www\.)?([a-zA-Z0-9.-]+)\.([a-z]{2,6})(\/[^\s]*)?$/,
+                      message: 'Invalid URL',
+                    },
+                  })}
+                  className='w-full px-4 py-2 border border-gray-300 rounded'
+                />
+                {errors.foodImage && (
+                  <p className='text-red-500 text-xs mt-1'>
+                    {errors.foodImage.message}
+                  </p>
+                )}
+              </div>
+
+              {/* Food Quantity */}
+              <div>
+                <label
+                  htmlFor='foodQuantity'
+                  className='block text-sm font-medium text-gray-700'
+                >
+                  Food Quantity
+                </label>
+                <input
+                  type='number'
+                  id='foodQuantity'
+                  defaultValue={food?.foodQuantity}
+                  {...register('foodQuantity', {
+                    required: 'Food quantity is required',
+                    min: 1,
+                  })}
+                  className='w-full px-4 py-2 border border-gray-300 rounded'
+                />
+                {errors.foodQuantity && (
+                  <p className='text-red-500 text-xs mt-1'>
+                    {errors.foodQuantity.message}
+                  </p>
+                )}
+              </div>
+
+              {/* Pickup Location */}
+              <div>
+                <label
+                  htmlFor='pickupLocation'
+                  className='block text-sm font-medium text-gray-700'
+                >
+                  Pickup Location
+                </label>
+                <input
+                  type='text'
+                  defaultValue={food?.pickupLocation}
+                  id='pickupLocation'
+                  {...register('pickupLocation', {
+                    required: 'Pickup location is required',
+                  })}
+                  className='w-full px-4 py-2 border border-gray-300 rounded'
+                />
+                {errors.pickupLocation && (
+                  <p className='text-red-500 text-xs mt-1'>
+                    {errors.pickupLocation.message}
+                  </p>
+                )}
+              </div>
+
+              {/* Expired Date */}
+              <div>
+                <label
+                  htmlFor='expiryDate'
+                  className='block text-sm font-medium text-gray-700'
+                >
+                  Expired Date
+                </label>
+                <DatePicker
+                  selected={expiryDate}
+                  onChange={(date) => setExpiryDate(date)}
+                  minDate={new Date()}
+                  //   value={new Date(food?.expiryDate).toLocaleDateString()}
+                  // showTimeSelect
+                  dateFormat='yyyy/MM/dd'
+                  className='w-full px-4 py-2 border border-gray-300 rounded'
+                />
+                {errors.expiryDate && (
+                  <p className='text-red-500 text-xs mt-1'>
+                    {errors.expiryDate.message}
+                  </p>
+                )}
+              </div>
+
+              {/* Additional Notes */}
+              <div>
+                <label
+                  htmlFor='additionalNotes'
+                  className='block text-sm font-medium text-gray-700'
+                >
+                  Additional Notes
+                </label>
+                <textarea
+                  id='additionalNotes'
+                  defaultValue={food?.additionalNotes}
+                  {...register('additionalNotes', {
+                    required: 'Additional notes is required',
+                  })}
+                  className='w-full px-4 py-2 border border-gray-300 rounded'
+                ></textarea>
+              </div>
+
+              {/* Submit Button */}
+              <div>
+                <button
+                  disabled={loading}
+                  type='submit'
+                  className='w-full bg-green-600 text-white py-2 rounded'
+                >
+                  Update Food
+                </button>
+              </div>
+            </form>
+            <div className='mt-4 flex justify-end'>
+              <button
+                onClick={() => setModalOpen(false)}
+                className='px-4 py-2 bg-gray-300 rounded mr-2'
+              >
+                Cancel
+              </button>
+              {/* <button
+                onClick={handleRequest}
+                className='px-4 py-2 bg-green-500 text-white rounded'
+              >
+                Request
+              </button> */}
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
