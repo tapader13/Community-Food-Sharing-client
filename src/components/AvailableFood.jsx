c1da778c8dcad5dd90dcd2f4f89ceacbb87cd792
tapader13 <minhajtapader0@gmail.com>
2024-12-22 22:33:14 -0500

    layout change in available page design

--- src/components/AvailableFood.jsx
+++ src/components/AvailableFood.jsx
@@ -9,6 +9,7 @@ const AvailableFood = () => {
   const [loading, setLoading] = useState(false);
   const [sortOption, setSortOption] = useState('');
   const navigate = useNavigate();
+  const [isThreeColumn, setIsThreeColumn] = useState(true);
 
   const fetchFoods = async (sortOption) => {
     try {
@@ -38,13 +39,27 @@ const AvailableFood = () => {
     }
   };
 
+  const toggleLayout = () => {
+    setIsThreeColumn(!isThreeColumn);
+  };
   useEffect(() => {
     fetchFoods(sortOption);
   }, [sortOption]);
 
   return (
     <div className='w-11/12 mx-auto p-4'>
-      <h2 className='text-2xl font-bold text-center mb-6'>Available Foods</h2>
+      <h2 className='text-2xl font-bold text-center mb-3'>
+        {' '}
+        <span className='text-green-600'>Available</span> Foods
+      </h2>
+      <div className='flex justify-end mb-4'>
+        <button
+          className='text-xl text-white w-fit px-4 py-2 cursor-pointer bg-green-600  border border-gray-300 rounded font-bold text-center'
+          onClick={toggleLayout}
+        >
+          Change Layout
+        </button>
+      </div>
 
       {/* Sort Section */}
       <div className='flex justify-between items-center mb-4'>
@@ -69,7 +84,11 @@ const AvailableFood = () => {
       {loading && <Spinner />}
 
       {/* Foods Section */}
-      <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+      <div
+        className={`grid grid-cols-1 md:grid-cols-${
+          isThreeColumn ? 3 : 2
+        } gap-4`}
+      >
         {foods.map((food) => (
           <div
             key={food._id}
@@ -78,7 +97,7 @@ const AvailableFood = () => {
             <img
               src={food.foodImage}
               alt={food.foodName}
-              className='w-full h-40 object-cover rounded-md mb-4'
+              className={`w-full h-48 object-cover rounded-md mb-4`}
             />
             <h3 className='text-lg font-bold'>{food.foodName}</h3>
             <p className='text-gray-700'>Quantity: {food.foodQuantity}</p>
