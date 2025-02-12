fef7dabf0f28e81ee70b1c3de57292ae081da517
tapader13 <minhajtapader0@gmail.com>
2025-02-12 00:23:24 -0500

    login page themed

--- src/components/authenticate/Login.jsx
+++ src/components/authenticate/Login.jsx
@@ -49,20 +49,20 @@ const Login = () => {
   };
 
   return (
-    <div className='min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
+    <div className='min-h-screen  flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
       <div className='sm:mx-auto sm:w-full sm:max-w-md'>
-        <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
+        <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-950 dark:text-gray-200'>
           Log in to your account
         </h2>
       </div>
 
       <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
-        <div className='bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10'>
+        <div className='bg-white dark:bg-gray-800 py-8 px-4 shadow sm:rounded-lg sm:px-10'>
           <form className='space-y-6' onSubmit={handleSubmit(onSubmit)}>
             <div>
               <label
                 htmlFor='email'
-                className='block text-sm font-medium text-gray-700'
+                className='block text-sm font-medium dark:text-gray-200 text-gray-700'
               >
                 Email address
               </label>
@@ -73,7 +73,7 @@ const Login = () => {
                   type='email'
                   autoComplete='email'
                   required
-                  className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+                  className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 text-gray-950 dark:text-white bg-white dark:bg-gray-800 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
                   {...register('email', {
                     required: 'Email is required',
                     pattern: {
@@ -93,7 +93,7 @@ const Login = () => {
             <div>
               <label
                 htmlFor='password'
-                className='block text-sm font-medium text-gray-700'
+                className='block text-sm font-medium dark:text-gray-200 text-gray-700'
               >
                 Password
               </label>
@@ -104,7 +104,7 @@ const Login = () => {
                   type='password'
                   autoComplete='current-password'
                   required
-                  className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
+                  className='appearance-none block w-full px-3 py-2 border border-gray-300 text-gray-950 dark:text-white bg-white dark:bg-gray-800 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm'
                   {...register('password', {
                     required: 'Password is required',
                   })}
@@ -134,7 +134,7 @@ const Login = () => {
                 <div className='w-full border-t border-gray-300' />
               </div>
               <div className='relative flex justify-center text-sm'>
-                <span className='px-2 bg-white text-gray-500'>
+                <span className='px-2 bg-white dark:bg-gray-800  text-gray-500'>
                   Or continue with
                 </span>
               </div>
@@ -156,7 +156,7 @@ const Login = () => {
                 <div className='w-full border-t border-gray-300' />
               </div>
               <div className='relative flex justify-center text-sm'>
-                <span className='px-2 bg-white text-gray-500'>
+                <span className='px-2 bg-white dark:bg-gray-800 text-gray-500'>
                   Don&apos;t have an account?
                 </span>
               </div>
@@ -165,7 +165,7 @@ const Login = () => {
             <div className='mt-6'>
               <Link
                 to='/register'
-                className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-green-600 bg-white hover:bg-gray-50'
+                className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium dark:bg-gray-700 text-green-600 bg-white hover:bg-gray-50'
               >
                 Register
               </Link>
